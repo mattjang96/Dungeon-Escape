3c4d00acfd6d37a918b155792aeca6a8fe56a008
mattjang96 <mattjang95@gmail.com>
2019-05-16 16:30:32 -0700

    OOP

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
 
-    const game = new Game(ctx);
-    const 
+
+     let game = new DEscape(ctx);
+    game.renderPreview();
 })
